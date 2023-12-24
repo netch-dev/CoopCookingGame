1c614e6b48246e3382be170e8a296efa22f0868e
Daijel <40652014+daijel@users.noreply.github.com>
2023-12-24 06:42:40 -0800

    organized scripts folder and added a trash counter to dispose of objects ingame

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,8 @@
+public class TrashCounter : BaseCounter
+{
+	public override void Interact(Player player) {
+		if (player.HasKitchenObject()) {
+			player.GetKitchenObject().DestroySelf();
+		}
+	}
+}
