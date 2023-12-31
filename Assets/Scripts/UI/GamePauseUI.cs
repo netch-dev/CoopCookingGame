5492c5c1e0980068b39e16b4f8ca89341c4c1c60
Daijel <40652014+daijel@users.noreply.github.com>
2023-12-31 06:13:40 -0800

    added an options menu with sound levels

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 public class GamePauseUI : MonoBehaviour {
 	[SerializeField] private Button resumeButton;
 	[SerializeField] private Button mainMenuButton;
+	[SerializeField] private Button optionsButton;
 	private void Awake() {
 		resumeButton.onClick.AddListener(() => {
 			KitchenGameManager.Instance.TogglePauseGame();
 		});
 		mainMenuButton.onClick.AddListener(() => {
 			Loader.Load(Loader.Scene.MainMenuScene);
+		});
+		optionsButton.onClick.AddListener(() => {
+			OptionsUI.Instance.Show();
 		});
 	}
 	private void Start() {
