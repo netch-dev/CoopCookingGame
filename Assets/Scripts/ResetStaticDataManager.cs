b15b53c6fc04229a3c20149d71db866934d58c4c
Daijel <40652014+daijel@users.noreply.github.com>
2023-12-30 17:06:29 -0800

    added a class to the main menu to cleanup the public static events for classes

--- Assets/Scripts/ResetStaticDataManager.cs
+++ Assets/Scripts/ResetStaticDataManager.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+
+// public static events for classes don't clear themselves when the scene changes so we have to reset them manually
+// example: public static event EventHandler OnAnyCut;
+
 public class ResetStaticDataManager : MonoBehaviour {
 	private void Awake() {
 		CuttingCounter.ResetStaticData();
